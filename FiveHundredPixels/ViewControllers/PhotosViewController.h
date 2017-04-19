675531ee81bfeda16bb0e0efbf96dbad4bbc453c
Ev Bogdanov <ev@bogdanov.io>
2017-04-19 22:53:10 +0300

    first commit

---
+++ FiveHundredPixels/ViewControllers/PhotosViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhotosViewController.h
+//  FiveHundredPixels
+//
+//  Created by Ev Bogdanov on 19/04/2017.
+//  Copyright © 2017 Ev Bogdanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotosViewController : UIViewController
+
+@end
---
+++ FiveHundredPixels/ViewControllers/PhotosViewController.m
@@ -0,0 +1,22 @@
+//
+//  PhotosViewController.m
+//  FiveHundredPixels
+//
+//  Created by Ev Bogdanov on 19/04/2017.
+//  Copyright © 2017 Ev Bogdanov. All rights reserved.
+//
+
+#import "PhotosViewController.h"
+
+@interface PhotosViewController ()
+
+@end
+
+@implementation PhotosViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"it works");
+}
+
+@end
